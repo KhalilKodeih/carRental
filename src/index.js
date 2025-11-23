cd44754b10058cd2bce02fd4052988811c4b9cda
Khalil Kodeih <khalilwkodeih@gmail.com>
2025-11-23 20:18:27 +0200

    Use HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./style.css";
 
+import { HashRouter } from "react-router-dom";
+
+<HashRouter>
+  <App />
+</HashRouter>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
