4f1486f5f321c59f51ebbf69a676bd78071d3472
Khalil Kodeih <khalilwkodeih@gmail.com>
2025-11-23 18:09:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// src/App.js
+import { Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Cars from "./pages/carListing";
+import CarDetails from "./pages/carDetails";
+import Contact from "./pages/contact";
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
